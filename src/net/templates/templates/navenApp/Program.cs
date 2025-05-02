d15119eaa53240f40ccc5d84ff33a952b3012398
masesdevelopers <94312179+masesdevelopers@users.noreply.github.com>
2025-05-02 15:21:44 +0200

    Finally clean netpdf name

--- src/net/templates/templates/navenApp/Program.cs
+++ src/net/templates/templates/navenApp/Program.cs
@@ -1,7 +1,4 @@
 ﻿using MASES.Naven;
-using Org.Apache.Pdfbox;
-using Org.Apache.Pdfbox.Io;
-using Org.Apache.Pdfbox.Pdmodel;
 using System;
 using System.IO;
 
@@ -9,36 +6,12 @@ namespace MASES.NavenTemplate.NavenApp
 {
     class Program
     {
-        class MyJNetCore : NavenCore<MyJNetCore> { }
+        class MyNavenCore : NavenCore<MyNavenCore> { }
 
         static void Main(string[] _)
         {
-            MyJNetCore.CreateGlobalInstance(); // this call prepares the environment: it is mandatory to initialize the JVM
-            var appArgs = MyJNetCore.FilteredArgs; // contains the remaining arguments: the Naven, JNet and JCOBridge arguments are discarded
-            if (appArgs.Length != 0)
-            {
-                Console.WriteLine($"Opening {appArgs[0]}");
-
-                using (var pdfObject = Loader.LoadPDF(new RandomAccessReadBufferedFile(appArgs[0]))) // open the PDF file referenced from command line: it is surrounded by using to execute the close on object at the end of operations
-                {
-                    // do stuff on opened PDF using pdfObject
-                    foreach (PDPage page in pdfObject.Pages)
-                    {
-                        // do stuff on pages of opened PDF using page variable
-                    }
-                    string outputFile;
-                    if (appArgs.Length > 1)
-                    {
-                        outputFile = appArgs[1];
-                    }
-                    else
-                    {
-                        outputFile = Path.Combine(Path.GetDirectoryName(appArgs[0]), $"{Path.GetFileNameWithoutExtension(appArgs[0])}_new.{Path.GetExtension(appArgs[0])}");
-                    }
-                    Console.WriteLine($"Saving on {outputFile}");
-                    pdfObject.Save(outputFile); // finally save the PDF object using the second parameter: writing on the original PDF file can create corruption
-                }
-            }
+            MyNavenCore.CreateGlobalInstance(); // this call prepares the environment: it is mandatory to initialize the JVM
+            var appArgs = MyNavenCore.FilteredArgs; // contains the remaining arguments: the Naven, JNet and JCOBridge arguments are discarded
         }
     }
 }
