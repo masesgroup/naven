6484f07d95a86ece583a9f2345c437aaf8bcdfda
masesdevelopers <94312179+masesdevelopers@users.noreply.github.com>
2025-07-20 17:26:25 +0200

    Fix

--- src/net/NavenPS/NavenPSCore.cs
+++ src/net/NavenPS/NavenPSCore.cs
@@ -38,6 +38,7 @@ namespace MASES.Naven.PowerShell
             get
             {
                 var lst = base.PathToParse;
+                lst.Add(Path.Combine(assembly.Location, "..", JARsSubFolder, "*.jar"));
                 lst.Add(Path.Combine(assembly.Location, "..", JARsSubFolder, Const.PlexusFolderName, "*.jar"));
                 lst.Add(Path.Combine(assembly.Location, "..", JARsSubFolder, Const.LibFolderName, "*.jar"));
                 return lst;
